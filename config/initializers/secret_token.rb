c17c1d7a7fd2f94192a1cdc2fb6ed267cc8b444d
Brian Bragg <brian.c.bragg@gmail.com>
2011-11-08 22:03:20 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stockit::Application.config.secret_token = '8aa1ef739133ebad0a78f27c1b0bbf5bab76362056c3c31f0b4cd80ccfcda0fbbb87f4ead76a93131574e92069d1ea652ad6866ee6605be7cb41bf98a6315f16'
