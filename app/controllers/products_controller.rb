edf17aff97da99ba63fbce01ae6fea3541a4ff02
Brian Bragg <brian.c.bragg@gmail.com>
2011-11-12 11:19:54 -0500

    working with the views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,8 @@ class ProductsController < ApplicationController
   # GET /products.json
   autocomplete :product_type, :prod_number, :full => true
   autocomplete :purchase, :po_number, :full => true
+
+  
   def index
     @products = Product.all
 
