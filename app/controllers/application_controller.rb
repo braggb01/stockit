69260febc4503e846974e63f049e384ae0c0411b
Brian Bragg <brian.c.bragg@gmail.com>
2012-01-20 17:40:45 -0500

    modifications to the view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def my_unfilled_po
+  	@myunfilled = Purchase.where("user_id = ? AND complete = ?", current_user, false)
+  end
+
+  helper_method :my_unfilled_po
+  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
